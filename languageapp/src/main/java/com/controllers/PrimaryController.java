19df988f7ad674a3ab5d215550059bb67556dc8c
graciedd <graciedd@email.sc.edu>
2024-11-14 09:35:49 -0500

    App to controllers

--- languageapp/src/main/java/com/controllers/PrimaryController.java
+++ languageapp/src/main/java/com/controllers/PrimaryController.java
@@ -1,11 +1,14 @@
-package com.chatterbox;
+package com.controllers;
+
 
 import java.io.IOException;
+
+import com.chatterbox.App;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
