8c7d0c5bd9ed4c9e31101dfd489fdceb2899498d
GMGustafson <gustafsg@email.sc.edu>
2024-11-14 09:14:25 -0500

    pom.xml

--- languageapp/src/main/java/module-info.java
+++ languageapp/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.chatterbox {
     requires javafx.controls;
-    requires javafx.fxml;
+    requires javafx.fxml; 
+    requires json.simple; 
+    requires jlayer; 
+    requires software
+
 
     opens com.chatterbox to javafx.fxml;
     exports com.chatterbox;
