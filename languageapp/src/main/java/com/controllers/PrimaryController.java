79f8dc7b82eba99fbd72aa1c0946a6ed4505d68a
graciedd <graciedd@email.sc.edu>
2024-11-14 10:32:18 -0500

    deleting PhraseList again

--- languageapp/src/main/java/com/controllers/PrimaryController.java
+++ languageapp/src/main/java/com/controllers/PrimaryController.java
@@ -1,10 +1,7 @@
 package com.controllers;
 
-<<<<<<< HEAD
-=======
 import java.io.IOException;
 import javafx.fxml.FXML; 
->>>>>>> 1600995c4a90e0b8d2ef0abacbe2bcc741e532dd
 import com.chatterbox.App;
 import java.io.IOException;
 import com.narration.*;
@@ -14,7 +11,7 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-    
+        Narriator.playSound("Hi my name is Grace Gustafson");
         App.setRoot("secondary");
     }
 }
