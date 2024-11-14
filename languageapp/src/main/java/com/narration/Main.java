dc2d82edf4a80f68d243f7a6dadd6a269453b31b
graciedd <graciedd@email.sc.edu>
2024-11-14 10:53:53 -0500

    adding lucia

--- languageapp/src/main/java/com/narration/Main.java
+++ languageapp/src/main/java/com/narration/Main.java
@@ -4,7 +4,7 @@ package com.narration;
 
 public class Main {
     public static void main(String[] args){
-        Narriator.playSound("Hi my name is Grace Gustafson");
+        Narriator.playSound("Hola Mundo");
        // UserInterface.playSound(); 
         
     }
