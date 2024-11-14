a65092011c3f0c0d349964e8a01f40a1288f2565
graciedd <graciedd@email.sc.edu>
2024-11-14 10:09:24 -0500

    pushing controllers

--- languageapp/src/main/java/com/controllers/PrimaryController.java
+++ languageapp/src/main/java/com/controllers/PrimaryController.java
@@ -1,13 +1,8 @@
 package com.controllers;
-<<<<<<< HEAD
-=======
-
->>>>>>> af367669b1117cc4c831c5c6a4cb8d55558fb7bd
 
 import java.io.IOException;
-
 import com.chatterbox.App;
-
+import com.narration.*;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
