e3dc5325bd3a17e068e55c80a99daf4613fa3386
graciedd <graciedd@email.sc.edu>
2024-11-14 10:26:57 -0500

    adding @FXML

--- languageapp/src/main/java/com/controllers/PrimaryController.java
+++ languageapp/src/main/java/com/controllers/PrimaryController.java
@@ -1,10 +1,5 @@
 package com.controllers;
 
-<<<<<<< HEAD
-import java.io.IOException;
-import javafx.fxml.FXML; 
-=======
->>>>>>> 887b5c48623ac388e53b844baa5ba6e0932e0d9a
 import com.chatterbox.App;
 import java.io.IOException;
 import com.narration.*;
@@ -12,7 +7,9 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+    @FXML
     private void switchToSecondary() throws IOException {
+    
         App.setRoot("secondary");
     }
 }
