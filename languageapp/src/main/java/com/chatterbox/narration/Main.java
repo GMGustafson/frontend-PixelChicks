61e384ddd146cf6d5639129fd7a037f0eeccd1a3
GMGustafson <gustafsg@email.sc.edu>
2024-11-14 09:21:45 -0500

    everything

---
+++ languageapp/src/main/java/com/chatterbox/narration/Main.java
@@ -0,0 +1,11 @@
+package com.chatterbox.narration;
+
+//import com.language.UserInterface;
+
+public class Main {
+    public static void main(String[] args){
+        Narriator.playSound("Hi my name is Grace Gustafson");
+       // UserInterface.playSound(); 
+        
+    }
+}
