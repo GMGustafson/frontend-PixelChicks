0fab1f42ed0cc9519bca068d6f0c22d43b7a6eb9
GMGustafson <gustafsg@email.sc.edu>
2024-11-14 10:30:34 -0500

    changes

--- languageapp/src/main/java/com/controllers/PrimaryController.java
+++ languageapp/src/main/java/com/controllers/PrimaryController.java
@@ -1,10 +1,7 @@
 package com.controllers;
 
-<<<<<<< HEAD
-=======
 import java.io.IOException;
 import javafx.fxml.FXML; 
->>>>>>> 1600995c4a90e0b8d2ef0abacbe2bcc741e532dd
 import com.chatterbox.App;
 import java.io.IOException;
 import com.narration.*;
