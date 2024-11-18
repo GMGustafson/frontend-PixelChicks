b97750e956bdfa3af7e78e9ce43dca2662a94fd7
graciedd <graciedd@email.sc.edu>
2024-11-18 10:13:30 -0500

    adding controllers and fxml

---
+++ languageapp/src/main/java/com/chatterbox/HomeController.java
@@ -0,0 +1,23 @@
+package com.chatterbox;
+
+//package com.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HomeController {
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
