e49fee19df07f0e44f9a53c0d1c1e7285165f834
GMGustafson <gustafsg@email.sc.edu>
2024-11-19 09:15:24 -0500

    grace stuff

--- languageapp/src/main/java/com/controllers/HomeController.java
+++ languageapp/src/main/java/com/controllers/HomeController.java
@@ -5,13 +5,9 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-
-import java.io.IOException;
-
+//import javafx.scene.control.TextField;
 import com.chatterbox.App;
-import com.narration.*;
-import javafx.fxml.FXML;
+
 
 public class HomeController {
 
@@ -22,15 +18,16 @@ public class HomeController {
     private Button loginButton;
 
     @FXML
+    void login(ActionEvent event) throws IOException{
+        System.out.println("Welcome back!");
+        App.setRoot("login");
+    }
+
+    @FXML
     void createAccount(ActionEvent event) throws IOException{
         System.out.println("Welcome. Let's create an account!");
         App.setRoot("createAccount");
     }
 
-    @FXML
-    void login(ActionEvent event) throws IOException{
-        System.out.println("Welcome back!");
-        App.setRoot("login");
-       
-    }
+   
 }
