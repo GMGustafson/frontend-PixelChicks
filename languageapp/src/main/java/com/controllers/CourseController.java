5cf3b8b7028b72a4dd19e70b28582c4fc20df56a
graciedd <graciedd@email.sc.edu>
2024-11-19 09:39:47 -0500

    adding icon

--- languageapp/src/main/java/com/controllers/CourseController.java
+++ languageapp/src/main/java/com/controllers/CourseController.java
@@ -1,5 +1,73 @@
 package com.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
+
 public class CourseController {
-    
+
+    @FXML
+    private MenuItem categoryColors;
+    @FXML
+    private MenuItem categoryNumbers;
+    @FXML
+    private MenuItem categoryPeople;
+    @FXML
+    private MenuItem categoryWeather;
+    @FXML
+    private MenuButton chooseCategories;
+    @FXML
+    private MenuButton chooseCourses;
+    @FXML
+    private MenuItem coursePhrases;
+    @FXML
+    private MenuItem courseWords;
+
+    @FXML
+    void chooseColors(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void chooseCourse(MouseEvent event) {
+
+    }
+
+    @FXML
+    void chooseNumbers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void choosePeople(ActionEvent event) {
+
+    }
+
+    @FXML
+    void choosePhrases(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chooseWeather(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chooseWords(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showCategories(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showCourses(MouseEvent event) {
+
+    }
+
 }
