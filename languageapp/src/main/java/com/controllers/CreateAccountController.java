273fd4ae7d2f02a012330fd52a45a5203bac5443
GMGustafson <gustafsg@email.sc.edu>
2024-11-19 09:33:44 -0500

    changes

--- languageapp/src/main/java/com/controllers/CreateAccountController.java
+++ languageapp/src/main/java/com/controllers/CreateAccountController.java
@@ -1,5 +1,9 @@
 package com.controllers;
 
+import java.io.IOException;
+
+import com.chatterbox.App;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,8 +14,9 @@ public class CreateAccountController {
     private Button next;
 
     @FXML
-    void goToCreateAccount2(ActionEvent event) {
-
+    void goToCreateAccount2(ActionEvent event) throws IOException{
+        System.out.println("going to next page"); 
+        App.setRoot("createAccount2");
     }
 
 }
