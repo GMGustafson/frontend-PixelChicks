876f8ba8e1914af6714e26b82287835ac15f0f4f
GMGustafson <gustafsg@email.sc.edu>
2024-11-19 20:59:08 -0500

    changes

--- languageapp/src/main/java/com/controllers/LoginController.java
+++ languageapp/src/main/java/com/controllers/LoginController.java
@@ -15,7 +15,7 @@ import javafx.scene.input.MouseEvent;
 import com.chatterbox.App;
 import com.model.*;
 
-public class LoginController implements Initializable {
+public class LoginController {
     @FXML
     private TextField txt_username;
     @FXML
@@ -39,26 +39,18 @@ public class LoginController implements Initializable {
         }
         else 
         { 
-           System.out.println("Valid login credentials.");
-            App.setRoot("home");
+            /* testing purposes */
+            lbl_error.setText("Welcome"  + username + "  " + password); 
+            
+
+           System.out.println("Valid login credentials." + username + "  " + password);
+            App.setRoot("courses");
         }
-        
-        //App.setRoot("user_home");
+    
     }
 
-    @FXML
-    private void btnLoginClicked(MouseEvent event) throws IOException {
-        
-    }
+    
 
-    @FXML
-    private void back(MouseEvent event) throws IOException {
-        App.setRoot("home");
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-    }
 
    
 }
