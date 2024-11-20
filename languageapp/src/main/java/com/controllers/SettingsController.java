760a285141eed2c0e4b8a09b316a1205e1d44b55
srisatishk <sriradhaas@gmail.com>
2024-11-19 20:37:18 -0500

    updates

---
+++ languageapp/src/main/java/com/controllers/SettingsController.java
@@ -0,0 +1,24 @@
+package com.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import com.chatterbox.App;
+
+public class SettingsController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
