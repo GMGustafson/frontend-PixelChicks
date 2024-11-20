0b5d0bf0b487ec8483144050c81c5b2e3c55b12f
graciedd <graciedd@email.sc.edu>
2024-11-19 20:25:39 -0500

    adding story

---
+++ languageapp/src/main/java/com/controllers/StoryController.java
@@ -0,0 +1,24 @@
+package com.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class StoryController {
+
+    @FXML
+    private Label englishText;
+
+    @FXML
+    private Button readStoryButton;
+
+    @FXML
+    private Label storyTitle;
+
+    @FXML
+    void readStoryAloud(ActionEvent event) {
+        
+    }
+
+}
