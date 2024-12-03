66ab0b9634dd08db3ca7b0ce597b53cbd984e1de
graciedd <graciedd@email.sc.edu>
2024-12-03 09:16:58 -0500

    flashcards

--- languageapp/src/main/java/com/controllers/CourseController.java
+++ languageapp/src/main/java/com/controllers/CourseController.java
@@ -1,87 +1,149 @@
 package com.controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
 import com.chatterbox.App;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import com.model.*;
 
-public class CourseController {
+public class CourseController implements Initializable{
+
 
     @FXML
-    private MenuItem categoryColors;
+    private Label lbl_welcome_user;
     @FXML
-    private MenuItem categoryNumbers;
+    private Category category_Colors;
     @FXML
-    private MenuItem categoryPeople;
+    private Category category_Numbers;
     @FXML
-    private MenuItem categoryWeather;
+    private Category category_People;
+    @FXML
+    private Category category_Weather;
     @FXML
     private MenuButton chooseCategories;
     @FXML
     private MenuButton chooseCourses;
     @FXML
-    private MenuItem coursePhrases;
+    private Course course_Phrases;
     @FXML
-    private MenuItem courseWords;
+    private Course course_Words;
     @FXML
-    private Button next2;
+    private Button nextToActivities;
+
+
+    private CourseList courseList;
+    private Category category;
+    private Course course;
+    private CategorySystemFacade facade;
+    private User user;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        courseList = CourseList.getInstance();
+        facade = CategorySystemFacade.getFacadeInstance();
+        user = facade.getCurrentUser();
+        lbl_welcome_user.setText("Welcome, " + user.getUsername() + ".");
+       
+        chooseCourse();
+        chooseCategory();
+       
+    }
+
+
+    //@FXML
+    private void chooseCourse() {
+       
+    }
+
+
+    //@FXML
+    private void chooseCategory() {
+        //List<String> categoryList = facade.getCategory();
+
+
+    }
+
 
     @FXML
-    void chooseColors(ActionEvent event) {
-        
+    void chooseColors(ActionEvent event) throws IOException {
+        Category chosenCategory = category_Colors;
+        facade.chooseCategory(chosenCategory);
     };
+
 
     @FXML
     void chooseCourse(MouseEvent event) {
 
+
     };
+
 
     @FXML
     void chooseNumbers(ActionEvent event) {
-
+        Category chosenCategory = category_Numbers;
+        facade.chooseCategory(chosenCategory);
     };
+
 
     @FXML
     void choosePeople(ActionEvent event) {
-
+        Category chosenCategory = category_People;
+        facade.chooseCategory(chosenCategory);
     };
+
 
     @FXML
     void choosePhrases(ActionEvent event) {
-
+        Course chosenCourse = course_Phrases;
+        facade.chooseCourse(chosenCourse);
     };
+
 
     @FXML
     void chooseWeather(ActionEvent event) {
+        Category chosenCategory = category_Weather;
+        facade.chooseCategory(chosenCategory);
+
 
     };
+
 
     @FXML
     void chooseWords(ActionEvent event) {
+        Course chosenCourse = course_Words;
+        facade.chooseCourse(chosenCourse);
+
 
     };
 
-    @FXML
-    void showCategories(ActionEvent event) {
 
+    @FXML
+    void showCategories(String category) {
+       
     }
 
     @FXML
     void showCourses(MouseEvent event) {
 
     }
-    
 
     @FXML
-    void GoToNext(ActionEvent event) throws IOException {
-        App.setRoot("matching");
-
+    void nextToActivities(ActionEvent event) throws IOException {
+            System.out.println("going to next page");
+            App.setRoot("activities");
     }
+
 
 }
--- languageapp/src/main/java/com/model/CategorySystemFacade.java
+++ languageapp/src/main/java/com/model/CategorySystemFacade.java
@@ -118,6 +118,11 @@ public class CategorySystemFacade {
         System.out.println("Course Choosen"+ this.course); 
         return this.course; 
     }
+    public Category chooseCategory(Category currentCategory) {
+        this.category = currentCategory;
+        System.out.println("Course Choosen"+ this.course); 
+        return this.category;
+    }
 
     public List<String> getCourse(){
         List<String> courseTitles = new ArrayList<>();
