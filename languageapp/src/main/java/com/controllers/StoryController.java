fd9440da7aad3a4568c4e9974707b716d52d76a3
GMGustafson <gustafsg@email.sc.edu>
2024-12-03 19:28:32 -0500

    grace-changes@7:28

--- languageapp/src/main/java/com/controllers/StoryController.java
+++ languageapp/src/main/java/com/controllers/StoryController.java
@@ -3,6 +3,7 @@ package com.controllers;
 import java.io.IOException;
 
 import com.chatterbox.App;
+import com.narration.Narriator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,6 +11,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class StoryController {
+
+    @FXML
+    private Label SpanishText;
+
+    @FXML
+    private Button back;
 
     @FXML
     private Label englishText;
@@ -21,18 +28,24 @@ public class StoryController {
     private Label storyTitle;
 
     @FXML
-    private Button back;
-
-
-    @FXML
-    void readStoryAloud(ActionEvent event) {
-        
+    void Translate(ActionEvent event) {
+        storyTitle.setText("Let's go grocery shopping!");
+        englishText.setText("At the store, Tom buys one red apple. He also looks for two ripe oranges. He finally buys three yellow bananas."); 
+        SpanishText.setText("En la tienda, Tom compra una manzana roja.También busca dos naranjas maduras.Finalmente compra tres plátanos amarillos."); 
     }
-
+    
     @FXML
     void backtoActivites(ActionEvent event) throws IOException {
         App.setRoot("activities");
     }   
 
+    @FXML
+    void readStoryAloud(ActionEvent event) {
+        Narriator.playSound("En la tienda, Tom compra una manzana roja.También busca dos naranjas maduras.Finalmente compra tres plátanos amarillos."); 
+    }
 
 }
+
+
+
+
