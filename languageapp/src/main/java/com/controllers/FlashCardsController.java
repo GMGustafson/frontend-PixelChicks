46974b7ed357b185e3a7d9945928b4ecbb8e37cf
srisatishk <sriradhaas@gmail.com>
2024-12-04 18:32:33 -0500

    updates

--- languageapp/src/main/java/com/controllers/FlashCardsController.java
+++ languageapp/src/main/java/com/controllers/FlashCardsController.java
@@ -1,14 +1,8 @@
+
 package com.controllers;
 
 import java.io.IOException;
 
-import com.chatterbox.App;
-import com.model.CategorySystemFacade;
-import com.model.Flashcard;
-
-
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,42 +11,19 @@ import javafx.scene.input.MouseEvent;
 
 public class FlashCardsController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+    @FXML
+    private Button Back;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+    @FXML
+    private Button word;
 
-    @FXML // fx:id="Back"
-    private Button Back; // Value injected by FXMLLoader
-
-    @FXML // fx:id="word"
-    private Button word; // Value injected by FXMLLoader
-
-    @FXML // fx:id="word"
-    private Button dorw; // Value injected by FXMLLoader
-    
     @FXML
     private Label wordLabel;
     private String[] words = {"rojo", "verde", "blanco"};
-    private String[] translated = {"red", "green", "white"};
     private int index = 0;
 
     @FXML
     void backtoActivites(ActionEvent event) {
-
-    }
-
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert Back != null : "fx:id=\"Back\" was not injected: check your FXML file 'FlashCards.fxml'.";
-        assert word != null : "fx:id=\"word\" was not injected: check your FXML file 'FlashCards.fxml'.";
-
-    }
-
-    @FXML
-    void GoToNext(ActionEvent event) throws IOException {
-        App.setRoot("activities");
 
     }
 
@@ -64,23 +35,16 @@ public class FlashCardsController {
 
     @FXML
     void translateCard(MouseEvent event) throws IOException{
-        word.setText(translated[index]);
-        index = (index + 1) % translated.length;
+        word.setText(words[index]);
+        index = (index + 1) % words.length;
     }
 
-    @FXML
-    void goBack(MouseEvent event) throws IOException{
-        if(index > 0){
-            index--;
-        }
-        else{
-           index = words.length - 1 ;
-        }
-        wordLabel.setText(words[index]);
-    }
+}
+  
+
 
     // private void showCard(){
     //     CategorySystemFacade facade = facade.getFacadeInstance();
     // }
 
-}
+
