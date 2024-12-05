e82da98824c352a5e8c9fbcb4bc78380a285a82f
srisatishk <sriradhaas@gmail.com>
2024-12-04 19:42:04 -0500

    updates

--- languageapp/src/main/java/com/controllers/FlashCardsController.java
+++ languageapp/src/main/java/com/controllers/FlashCardsController.java
@@ -2,25 +2,34 @@
 package com.controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.model.CategorySystemFacade;
+import com.model.Flashcard;
+import com.model.User;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
-public class FlashCardsController {
-
+public class FlashCardsController implements Initializable {
     @FXML
     private Button Back;
-
     @FXML
     private Button forward;
-
     @FXML
     private Label wordLabel;
+    @FXML
+    private Label showcard;
     private String[] words = {"rojo", "verde", "blanco"};
     private int index = 0;
+    private CategorySystemFacade facade;
+    private User user;
+    private Flashcard flashcard;
 
     @FXML
     void backtoActivites(ActionEvent event) {
@@ -43,6 +52,15 @@ public class FlashCardsController {
     void translateCard(MouseEvent event) throws IOException{
         forward.setText(words[index]);
         index = (index + 1) % words.length;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        facade = CategorySystemFacade.getFacadeInstance();
+        user = facade.getCurrentUser();
+        
+        showcard.setText("Welcome " + user.getFirstName() + " " + user.getLastName());
+        
     }
 
 }
