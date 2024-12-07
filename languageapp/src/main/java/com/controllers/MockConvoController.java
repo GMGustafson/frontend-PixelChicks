2efa7bc07870ed638ce960a1f55ebff3ae9b8c2f
srisatishk <sriradhaas@gmail.com>
2024-12-07 12:46:58 -0500

    update

--- languageapp/src/main/java/com/controllers/MockConvoController.java
+++ languageapp/src/main/java/com/controllers/MockConvoController.java
@@ -19,7 +19,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
